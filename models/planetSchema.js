e9ec1d77a3be4dd8e493794c61c427d68ad78369
Mohammad Khalida Parveen <mohammadkhalidaparveen23@gmail.com>
2023-11-28 15:40:57 -0600

    created a project

--- models/planetSchema.js
+++ models/planetSchema.js
@@ -1,8 +1,19 @@
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const planetSchema = mongoose.Schema({
-name: String,
-size: Number,
-speed: Number
-})
-module.exports = mongoose.model("planet",
-planetSchema)
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    speed: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 890000
+    }
+});
+module.exports = mongoose.model("planet",planetSchema)
--- routes/planet.js
+++ routes/planet.js
@@ -1,7 +1,15 @@
 var express = require('express');
 const planet_controlers= require('../controllers/planet');
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 router.get('/', planet_controlers.planet_view_all_Page );
 
 
@@ -12,7 +20,8 @@ router.get('/detail', planet_controlers.planet_view_one_Page);
 router.get('/create', planet_controlers.planet_create_Page);
 
 /* GET create update page */
-router.get('/update', planet_controlers.planet_update_Page);
+/* GET update planet page */
+router.get('/update', secured,planet_controlers.planet_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', planet_controlers.planet_delete_Page);
